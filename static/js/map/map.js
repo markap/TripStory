ffc49c209e2c87cad5740032cb44adf46bd851eb
Martin <mart.kapfhammer@gmail.com>
2014-10-12 11:13:32 +0200

    search box

--- static/js/map/map.js
+++ static/js/map/map.js
@@ -30,6 +30,66 @@ angular.module('tripStoryApp.map', ['ngRoute'])
     var directionsService = new google.maps.DirectionsService();
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+    var currentMarker = null;
+
+    // Create the search box and link it to the UI element.
+    var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
+        console.log(places);
+
+        $scope.currentPosition = -1;
+        $scope.trip.description = '';
+
+        $scope.setMarkersInactive();
+
+        if (places.length == 0) {
+          return;
+        }
+
+        if (currentMarker) {
+            currentMarker.setMap(null);
+        }
+
+        var bounds = new google.maps.LatLngBounds();
+
+        // Create a marker for each place.
+        currentMarker = new google.maps.Marker({
+            map: map,
+            title: places[0].name,
+            position: places[0].geometry.location
+        });
+
+
+
+        bounds.extend(places[0].geometry.location);
+        if (bounds.getNorthEast().equals(bounds.getSouthWest())) {
+          var extendPoint = new google.maps.LatLng(bounds.getNorthEast().lat() + 2, bounds.getNorthEast().lng() + 2);
+          bounds.extend(extendPoint);
+        }
+        map.fitBounds(bounds);
+
+        $scope.$apply();
+
+    });
+
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+    });
+
+
     // Create a renderer for directions and bind it to the map.
     var rendererOptions = {
       map: map,
@@ -67,6 +127,8 @@ angular.module('tripStoryApp.map', ['ngRoute'])
       }
     };
 
+
+
     google.maps.event.addListener(map, 'click', function(e) {
 
       $scope.currentPosition = -1;
@@ -82,7 +144,7 @@ angular.module('tripStoryApp.map', ['ngRoute'])
       currentMarker = new google.maps.Marker({
         position: e.latLng,
         map: map,
-        title: 'Hello World!'
+        title: ''
       });
       $scope.$apply();
     });
